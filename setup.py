9fbf3786dd6c76ca95f77de666a76d4626ad7390
CahunH <116684250+CahunH@users.noreply.github.com>
2024-08-06 19:34:23 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='password_manager',
+    version='1.0',
+    description='Password Manager CLI Tool',
+    author='DavidH',
+    author_email='davidhvcahun@hotmail.com',
+    packages=['app'],
+    install_requires=[
+        # Lista de dependencias, si es necesario
+        # 'requests>=2.25.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'psswrd=app.main:main',
+        ],
+    },
+)
